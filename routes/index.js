1d8cefd74eeeb90664a4fb5b26512fb7cfb5e44f
Clyde Wang <sunclyde@qq.com>
2018-09-01 17:59:10 +0800

    add opencv

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('opencv', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/opencv', function(req, res, next) {
+  res.render('opencv', { title: 'Express' });
 });
 
 module.exports = router;
