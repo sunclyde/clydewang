05fe831fbe2e05845fb3def971c05f8b4eaf5603
Clyde Wang <sunclyde@gmail.com>
2020-01-03 23:36:36 +0800

    add cards sample

--- routes/library.js
+++ routes/library.js
@@ -3,10 +3,89 @@ var router = express.Router();
 var Page = require('../app/core/Page');
 
 /* GET card library. */
-router.get('/card', function(req, res, next) {
+router.get('/card', function (req, res, next) {
 	page = new Page();
 	page.ns = 'library';
-	res.render('library/card', {page: page});
+	page.categories = [
+		{
+			name: "Attack"
+		},
+		{
+			name: "Heal"
+		},
+		{
+			name: "Buff"
+		},
+		{
+			name: "Debuff"
+		},
+		{
+			name: "Summon"
+		},
+	];
+	page.cards = [
+		{
+			name: "Card1",
+			rare: "N",
+			description: "This is a card",
+		},
+		{
+			name: "Card2",
+			rare: "N",
+			description: "This is a card",
+		},
+		{
+			name: "Card3",
+			rare: "N",
+			description: "This is a card",
+		},
+		{
+			name: "Card4",
+			rare: "R",
+			description: "This is a card",
+		},
+		{
+			name: "Card5",
+			rare: "R",
+			description: "This is a card",
+		},
+		{
+			name: "Card6",
+			rare: "R",
+			description: "This is a card",
+		},
+		{
+			name: "Card7",
+			rare: "SR",
+			description: "This is a card",
+		},
+		{
+			name: "Card8",
+			rare: "SR",
+			description: "This is a card",
+		},
+		{
+			name: "Card9",
+			rare: "SR",
+			description: "This is a card",
+		},
+		{
+			name: "Card10",
+			rare: "SSR",
+			description: "This is a card",
+		},
+		{
+			name: "Card11",
+			rare: "SSR",
+			description: "This is a card",
+		},
+		{
+			name: "Card12",
+			rare: "SSR",
+			description: "This is a card",
+		}
+	]
+	res.render('library/card', { page: page });
 });
 
 module.exports = router;
